418cd168ab61e243544c896293b121ff305225b4
Muhammad Sufi "CRYTLZE" Aulia <41355094+crytlze@users.noreply.github.com>
2019-01-10 00:33:44 +0700

    Versi 01
    Tahap Perbaikan

---
+++ app/src/main/java/com/khilman/www/aplikasiportalberita/LihatDataUser.java
@@ -0,0 +1,47 @@
+package com.khilman.www.aplikasiportalberita;
+
+import android.database.Cursor;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LihatDataUser extends AppCompatActivity {
+
+    protected Cursor cursor;
+    SqliteHelper dbHelper;
+    Button ton2;
+    TextView text1, text2, text3, text4, text5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lihat_data_user);
+
+        dbHelper = new SqliteHelper(this);
+        text1 = (TextView) findViewById(R.id.textView1);
+        text2 = (TextView) findViewById(R.id.textView2);
+        text3 = (TextView) findViewById(R.id.textView3);
+        text4 = (TextView) findViewById(R.id.textView4);
+
+
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        cursor = db.rawQuery("SELECT * FROM users WHERE username = '" +
+                getIntent().getStringExtra("nama") + "'",null);
+        cursor.moveToFirst();
+        if (cursor.getCount()>0)
+        {
+            cursor.moveToPosition(0);
+            text1.setText(cursor.getString(0).toString());
+            text2.setText(cursor.getString(1).toString());
+            text3.setText(cursor.getString(2).toString());
+            text4.setText(cursor.getString(3).toString());
+        }
+
+    }
+}
